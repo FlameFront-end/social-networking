b5c5ff52a0ba20a0c07a2cbd5da8ca23100f856d
Flame <5017_30@mail.ru>
2023-04-04 21:14:41 +0300

    init: start project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import Technologies from "./components/Technologies";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+      <div>
+          <header>
+
+          </header>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/Technologies.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Technologies = () => {
+    return (
+        <div>Technologies</div>
+    )
+}
+export default Technologies
